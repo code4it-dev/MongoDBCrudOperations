4b12374474a3eaf0a5ccb2bcf5bddc0d81b8f017
Davide Bellone <davide.bellone>
2020-10-14 21:50:51 +0200

    Finished demo project and code cleanup

--- BoardGameAPI/Controllers/BoardGamesController.cs
+++ BoardGameAPI/Controllers/BoardGamesController.cs
@@ -1,6 +1,5 @@
 ﻿using BoardGameAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -33,24 +32,32 @@ namespace BoardGameAPI.Controllers
             return Ok(allDocuments.ToList());
         }
 
-        //[HttpGet]
-        //[Route("{id}")]
-        //public async Task<ActionResult> GetById(string id)
-        //{
-        //    var filter = Builders<Game>.Filter;
-        //    var eqFilter = filter.Eq(x => x.Id, new ObjectId(id));
-        //    var game = await dbCollection.FindAsync<Game>(eqFilter).ConfigureAwait(false);
-        //    return Ok(game.FirstOrDefault());
-        //}
-
         [HttpGet]
-        [Route("byname/{name}")]
-        public async Task<ActionResult> GetByName(string name)
+        [Route("{id}")]
+        public async Task<ActionResult> GetById(string id)
         {
             var filter = Builders<Game>.Filter;
-            var eqFilter = filter.Eq(x => x.Name, name);
+            var eqFilter = filter.Eq(x => x.AnotherId, id);
+
+            /*NOTE: If you prefer the native ObjectId, here's how you can query Mongo using that object type*/
+            // var eqFilter = filter.Eq(x => x.Id, new ObjectId(id));
+
             var game = await dbCollection.FindAsync<Game>(eqFilter).ConfigureAwait(false);
             return Ok(game.FirstOrDefault());
+        }
+
+        [HttpGet]
+        [Route("byplayers/{players}")]
+        public async Task<ActionResult> GetByName(int players)
+        {
+            var filter = Builders<Game>.Filter;
+            var minNumberFilter = filter.Lte(x => x.MinNumberOfPlayers, players);
+            var maxNumberFilter = filter.Gte(x => x.MaxNumberOfPlayers, players);
+
+            var finalFilter = filter.And(minNumberFilter, maxNumberFilter);
+
+            var game = await dbCollection.FindAsync<Game>(finalFilter).ConfigureAwait(false);
+            return Ok(game.ToList());
         }
 
         [HttpPost]
@@ -60,5 +67,45 @@ namespace BoardGameAPI.Controllers
             dbCollection.InsertOne(newGame);
             return Ok();
         }
+
+        [HttpPatch]
+        public async Task<ActionResult> Update([FromQuery] string gameId, [FromQuery] string newName)
+        {
+            FilterDefinitionBuilder<Game> eqfilter = Builders<Game>.Filter;
+            FilterDefinition<Game> eqFilterDefinition = eqfilter.Eq(x => x.AnotherId, gameId);
+
+
+            UpdateDefinitionBuilder<Game> updateFilter = Builders<Game>.Update;
+            UpdateDefinition<Game> updateFilterDefinition = updateFilter.Set(x => x.Name, newName);
+
+            UpdateResult updateResult = await dbCollection.UpdateOneAsync(eqFilterDefinition, updateFilterDefinition).ConfigureAwait(false);
+
+            if (updateResult.ModifiedCount > 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete([FromQuery] string gameId)
+        {
+            FilterDefinitionBuilder<Game> filter = Builders<Game>.Filter;
+            FilterDefinition<Game> eqFilter = filter.Eq(x => x.AnotherId, gameId);
+            DeleteResult res = await dbCollection.DeleteOneAsync(eqFilter).ConfigureAwait(false);
+
+            if (res.DeletedCount > 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
     }
 }
